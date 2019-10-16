3861c95f86ad89ac797dd0a77b37e61718d3020f
Carlos Jaramillo <cjaramillo@ias.com.co>
2019-10-16 09:50:22 -0500

    pruebas unitarias básicas

---
+++ src/app/app.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from './models/user.interface';
+import { UserService } from './services/user.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  myVar = 'Hola Mundo'
+  saludo = 'Buenos dias Carlos, como te encuentras?'
+  users: User[] = []
+
+
+  constructor(private _userService: UserService) {}
+
+  ngOnInit(){
+    this.getUsers()
+  }
+
+  par(numero: number): boolean{
+    return numero % 2 === 0 ? true : false
+  }
+
+  getUsers(){
+    this._userService.getAll().subscribe(users => {
+      this.users = users
+      console.log(this.users)
+    })
+  }
+}
